96d843fa8f1b04189fbd8e0a11d1a68c3c1ed77f
zeing <zqguo92@gmail.com>
2014-03-09 23:50:46 +0800

    fix conflict

--- pylab1/views.py
+++ pylab1/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-<<<<<<< HEAD
 from numpy import *
 from matplotlib.pylab import *
 from matplotlib.cbook import *
@@ -9,11 +8,6 @@ from matplotlib.patches import Ellipse
 from mpl_toolkits.mplot3d import Axes3D
 from util import mimeTypeDict
 from django.conf import settings
-=======
-from numpy import abs, cos, sin, linspace, arange, pi, exp, random
-from matplotlib.pylab import figure, plot, xlabel, ylabel, title, xlim, ylim, legend, show, savefig, subplot, hist, text, axis, grid, gcf
-from util import mimeTypeDict
->>>>>>> 572ea2b005db99e2deeae8ad0ecb7bd12235e50c
 
 import cStringIO
 import Image
@@ -28,18 +22,11 @@ def help(request):
     return render_to_response("help.html")
         
 def ajax(request):
-<<<<<<< HEAD
     returnInfo = {}
     try:
         query = ""
         mimeType = ""
     	imgData = cStringIO.StringIO()
-=======
-    try:
-        query = ""
-        mimeType = ""
-        savePath = ""
->>>>>>> 572ea2b005db99e2deeae8ad0ecb7bd12235e50c
     
         # Test and get npQuery and pltQuery from request
         if (not request.method == "POST") or not ("query" in request.POST or "mimeType" in request.POST or "fileName" in request.POST):
@@ -49,13 +36,8 @@ def ajax(request):
         query = request.POST["query"]
     
         # Make sure that the input instruction is valid 
-<<<<<<< HEAD
         if not checkScurity(query):
             return HttpResponse("Invalid characters!")
-=======
-        #if not checkScurity(query):
-        #    return HttpResponse("发现非法字符")
->>>>>>> 572ea2b005db99e2deeae8ad0ecb7bd12235e50c
     
         # Split the query and execute thme
         split = re.compile("&%&")
@@ -65,7 +47,6 @@ def ajax(request):
             exec(queryItem)
         
         # Save the image
-<<<<<<< HEAD
         savefig(imgData, format = mimeType)
         # Clear the streamIO of matplotlib.pylib
         gcf().clear()
@@ -130,23 +111,3 @@ def checkScurity(query):
         return False
     else:
         return True
-=======
-        savePath = "media/save/%s.%s" % (fileName, mimeType)
-        savefig(savePath)
-        
-        # Clear the streamIO of matplotlib.pylib
-        gcf().clear()
-    
-        # Response
-        payload = {"url": savePath, "format": mimeType}
-    except Exception,data:
-        print Exception,":",data
-    return HttpResponse(json.dumps(payload), mimetype="application/json")
-
-def checkScurity(query):
-    semicolon = '%'
-    if query.find(semicolon):
-        return False
-    else:
-        return True
->>>>>>> 572ea2b005db99e2deeae8ad0ecb7bd12235e50c
